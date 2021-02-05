529377e4db33d905354eeecab36acb6cc92f3df7
Gianpaolo <pao.barga@gmail.com>
2021-02-05 17:34:09 +0100

    modificato bonus: tolto settimeout

--- js/script.js
+++ js/script.js
@@ -4,12 +4,13 @@ const emailsQuantity = 10;
 // VUE
 new Vue({
     el: "#root",
+
     data: {
         emailsSelf: [],
         emailsArrow: [],
     }, 
+
     mounted: function() {
-        // const emailsQuantity = 10; 
 
         // using self
         const self = this;
@@ -33,15 +34,14 @@ new Vue({
 
 // BONUS: JQUERY
 $(document).ready(function() {
-    let emailsJQuery = "";
-    
+
     for(let i = 0; i < emailsQuantity; i++) {
 
         $.ajax({
             url: "https://flynn.boolean.careers/exercises/api/random/mail",
             method: "GET",
             success: function(data) {
-                emailsJQuery += `<li>${data.response}</li>`
+                document.querySelector(".email-list.jquery").innerHTML += `<li>${data.response}</li>`
             },
             error: function(request, status, error) {
                 document.querySelector(".email-list.jquery").innerHTML = `Request: ${request}, status: ${status}, error: ${error}`;
@@ -50,25 +50,16 @@ $(document).ready(function() {
         
     }
     
-    setTimeout(() => {
-        document.querySelector(".email-list.jquery").innerHTML = emailsJQuery; 
-    }, 2000) 
 })
 
 // BONUS: VANILLA
-var emailsVanilla = "";
-
 for(let i = 0; i < emailsQuantity; i++) {
     let xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if(this.readyState === 4 && this.status === 200) {
-            emailsVanilla += `<li>${JSON.parse(xhttp.responseText).response}</li>`;
+            document.querySelector(".email-list.vanilla").innerHTML += `<li>${JSON.parse(xhttp.responseText).response}</li>`;
         }
     }
     xhttp.open("GET", "https://flynn.boolean.careers/exercises/api/random/mail", true);
     xhttp.send();
 }
-
-setTimeout(() => {
-    document.querySelector(".email-list.vanilla").innerHTML = emailsVanilla;
-}, 1000)
